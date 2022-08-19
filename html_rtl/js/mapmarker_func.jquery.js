5d82dc5dcbaa98419259e0e1444d8603abf687ce
paulinacetd <45112568+paulinacetd@users.noreply.github.com>
2022-08-19 12:57:39 -0500

    Update mapmarker_func.jquery.js

--- html_rtl/js/mapmarker_func.jquery.js
+++ html_rtl/js/mapmarker_func.jquery.js
@@ -1,13 +1,13 @@
 		//set up markers 
 		var myMarkers = {
 		    "markers": [
-		        { "latitude": "21.16163", "longitude": "-86.83498", "icon": "img/map-marker.png" }
+		        { "latitude": "51.511732", "longitude": "-0.123270", "icon": "img/map-marker.png" }
 		    ]
 		};
 
 		//set up map options
 		$(".map_contact").mapmarker({
 		    zoom: 14,
-		    center: 'Plaza Arrecife 28',
+		    center: 'Covent Garden London',
 		    markers: myMarkers
 		});
